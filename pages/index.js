66825a838aa17c1290231d07374f0fa90dde5666
Romero Gonçalves <romerotg@gmail.com>
2025-01-04 15:36:05 +0000

    adding scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 function Home() {
-    return (
-        <>
-            <h1>NH</h1>
-            <img src="NH cortado.png" alt="NH" />
-        </>
-    )
+  return (
+    <>
+      <h1>NH</h1>
+      <img src="NH cortado.png" alt="NH" />
+    </>
+  );
+}
+
+function teste() {
+  console.log("indentação errada");
 }
 
 export default Home;
